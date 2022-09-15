662e6346eb7deaa66626ea0c398915a7d6e99707
yaofahua <yaofahua@hilisee.com>
2022-09-15 18:13:19 +0800

    feat: Convert mediawiki to markdown

--- src/extension.ts
+++ src/extension.ts
@@ -6,51 +6,57 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext)
 {
-
-    /**
-     * 判断字符串是否为空或undefined,不判断为0,不判断为false
-     * @param str
-     * @returns {boolean}
-     */
-    function isEmpty(str: any): boolean
-    {
-        if (str === null || str === '' || str === undefined || str.length === 0) {
-            return true
-        } else {
-            return false
-        }
-    };
-
-    // 临时文件夹
-    var tempDir = os.tmpdir();
-    var tempFile = path.normalize(path.join(tempDir, 'mytools-temp.txt'));
-
     // 打开系统剪贴板中路径指向的文件
+    registerCommandOpenFile(context);
+
+    // markdown格式文本转化成mediawiki格式
+    registerCommandMarkdownToMediawiki(context);
+
+    // mediawiki格式文本转化成markdown格式
+    registerCommandMediawikiToMarkdown(context)
+}
+
+/**
+ * 判断字符串是否为空或undefined,不判断为0,不判断为false
+ * @param str
+ * @returns {boolean}
+ */
+function isEmpty(str: any): boolean
+{
+    if (str === null || str === '' || str === undefined || str.length === 0) {
+        return true
+    } else {
+        return false
+    }
+};
+
+function registerCommandOpenFile(context: vscode.ExtensionContext)
+{
     context.subscriptions.push(vscode.commands.registerCommand('extension.my-openfile', async () => {
         const txt = await vscode.env.clipboard.readText();
         const doc = await vscode.workspace.openTextDocument(vscode.Uri.file(txt));
         vscode.window.showTextDocument(doc);
     }));
+}
 
-    // markdown格式文本转化成mediawiki格式（当前编辑窗口 -> 剪贴板）
+function registerCommandMarkdownToMediawiki(context: vscode.ExtensionContext)
+{
+    // 临时文件夹
+    var tempDir = os.tmpdir();
+    var tempFile = path.normalize(path.join(tempDir, 'mytools-temp.txt'));
+
     context.subscriptions.push(vscode.commands.registerCommand('extension.my-md2mediawiki', async () => {
-        // 当前编辑窗口
-        var editor = vscode.window.activeTextEditor;
-        if (editor == undefined) {
-            return;
-        }
+		// 系统剪贴板内容
+		const txt = await vscode.env.clipboard.readText();
+		if (isEmpty(txt)) {
+			return;
+		}
 
-        // 当前选定内容
-        var selectText = editor.document.getText(editor.selection);
-        if (isEmpty(selectText)) {
-            return;
-        }
-
-        // 当前选定内容到临时文件
-        fs.writeFileSync(tempFile, selectText);
+        // 剪贴板内容到临时文件
+        fs.writeFileSync(tempFile, txt);
 
         // pandoc转换
-        var targetExec = 'pandoc --from markdown_github --to mediawiki ' + tempFile;
+        var targetExec = 'pandoc --from gfm --to mediawiki ' + tempFile;
         var child = exec(targetExec, {cwd : tempDir}, function(error, stdout, stderr) {
             if (error != null) {
                 vscode.window.showErrorMessage(error.message + '\n' + targetExec);
@@ -62,8 +68,14 @@ export function activate(context: vscode.ExtensionContext)
             }
         });
     }));
+}
 
-    // mediawiki格式文本转化成markdown格式（剪贴板 -> 剪贴板）
+function registerCommandMediawikiToMarkdown(context: vscode.ExtensionContext)
+{
+    // 临时文件夹
+    var tempDir = os.tmpdir();
+    var tempFile = path.normalize(path.join(tempDir, 'mytools-temp.txt'));
+
     context.subscriptions.push(vscode.commands.registerCommand('extension.my-mediawiki2md', async () => {
 
 		// 系统剪贴板内容
@@ -99,3 +111,5 @@ export function activate(context: vscode.ExtensionContext)
         });
     }));
 }
+
+ 
