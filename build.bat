9baa3a9327a0ccc5e444f53c87d8bcda08ced6fb
yaofahua <yaofahua@hilisee.com>
2022-09-29 14:57:31 +0800

    feat: Update

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo === compile ===
 call yarn compile
 
 echo === package ===
-call vsce package -o dist
+call yarn run vsce package -o dist
 
 echo === install ===
 call code --install-extension dist\mytools-1.0.0.vsix
--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,25 @@
-import {exec, spawn} from 'child_process';
-import * as fs from 'fs'
-import * as os from 'os'
-import * as path from 'path';
-import * as vscode from 'vscode';
-import * as util from './myutil'
+import { exec, spawn } from "child_process";
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+import * as vscode from "vscode";
+import * as util from "./myutil";
 
-export function activate(context: vscode.ExtensionContext)
-{
-    // 打开系统剪贴板中路径指向的文件
-    registerCommandOpenFile(context);
+export function activate(context: vscode.ExtensionContext) {
+  // 打开系统剪贴板中路径指向的文件
+  registerCommandOpenFile(context);
 
-    // markdown格式文本转化成mediawiki格式
-    registerCommandMarkdownToMediawiki(context);
+  // mediawiki格式文本转化成markdown格式
+  registerCommandMediawikiToMarkdown(context);
 
-    // mediawiki格式文本转化成markdown格式
-    registerCommandMediawikiToMarkdown(context)
+  // 创建临时文件
+  registerCommandCreateAndOpenTempFile(context);
 
-    // 创建临时文件
-    registerCommandCreateAndOpenTempFile(context)
+  // 打开指定目录的文件或文件夹
+  registerCommandOpenFaviFilesOrFolder(context);
 
-    // 打开memo.txt 1.txt 2.txt
-    registerCommandOpenMemoFile(context)
-    
-    // 打开指定目录的文件或文件夹
-    registerCommandOpenFaviFilesOrFolder(context)
+  // 用Fork打开文件夹
+  registerCommandOpenWithFork(context);
 }
 
 /**
@@ -31,180 +27,228 @@ export function activate(context: vscode.ExtensionContext)
  * @param str
  * @returns {boolean}
  */
-function isEmpty(str: any): boolean
-{
-    if (str === null || str === '' || str === undefined || str.length === 0) {
-        return true
-    } else {
-        return false
-    }
-};
+function isEmpty(str: any): boolean {
+  if (str === null || str === "" || str === undefined || str.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-function registerCommandOpenFaviFilesOrFolder(context: vscode.ExtensionContext)
-{
-    context.subscriptions.push(vscode.commands.registerCommand('extension.my-openFaviFileOrFolder', async () => {
-        //
-        // 关注的路径，true表示递归子目录且只找文件，false表示只查找一级文件夹
-        //
-        var paths = [ [ "C:\\Users\\hw\\Desktop\\mydoc\\docs\\", true ], [ "C:\\Users\\hw\\Desktop\\src\\", false ] ];
-        
-        // 所有指定的文件及文件夹
-        var filesOrFolders: string[] = [];
-        for (var i = 0; i < paths.length; i++) {
-            var path = paths[i];
+function registerCommandOpenWithFork(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "mytools.openWithFork",
+      (fileUri: vscode.Uri) => {
+        util.getMyTerminal().sendText("fork " + fileUri.fsPath);
+      }
+    )
+  );
+}
 
-            // 递归子目录的所有文件
-            if (path[1]) {
-                var files = await util.fileSearch(<string>path[0], false, true, false);
-                filesOrFolders = filesOrFolders.concat(files);
+function registerCommandOpenFaviFilesOrFolder(
+  context: vscode.ExtensionContext
+) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("mytools.openFaviFileOrFolder", async () => {
+      //
+      // 关注的路径，true表示递归子目录且只找文件，false表示只查找一级文件夹
+      //
+      const paths = [
+        ["C:\\Users\\hw\\Desktop\\mydoc\\docs\\", true],
+        ["C:\\Users\\hw\\Desktop\\src\\", false],
+      ];
 
-            } else { // 只查找一级文件夹
-                var files2 = await util.fileSearch(<string>path[0], true, false, true);
-                filesOrFolders = filesOrFolders.concat(files2);
-            }
+      // 所有指定的文件及文件夹
+      let filesOrFolders: string[] = [];
+      for (let i = 0; i < paths.length; i++) {
+        const path = paths[i];
+
+        // 递归子目录的所有文件
+        if (path[1]) {
+          const files = await util.fileSearch(
+            <string>path[0],
+            false,
+            true,
+            false
+          );
+          filesOrFolders = filesOrFolders.concat(files);
+        } else {
+          // 只查找一级文件夹
+          const files2 = await util.fileSearch(
+            <string>path[0],
+            true,
+            false,
+            true
+          );
+          filesOrFolders = filesOrFolders.concat(files2);
         }
+      }
 
-        // 快速选择
-        var fileOrFolder = await vscode.window.showQuickPick(filesOrFolders);
+      // 快速选择
+      const fileOrFolder = await vscode.window.showQuickPick(filesOrFolders);
 
-        // 当前窗口打开文件，新窗口打开文件夹
-        var stat = fs.statSync(<string>fileOrFolder);
-        if (stat.isFile()) {
-            var doc = await vscode.workspace.openTextDocument(vscode.Uri.file(<string>fileOrFolder));
-            vscode.window.showTextDocument(doc);    
-        } else if (stat.isDirectory()) {
-            util.getMyTerminal().sendText(`code ${fileOrFolder}`);
+      // 当前窗口打开文件，新窗口打开文件夹
+      const stat = fs.statSync(<string>fileOrFolder);
+      if (stat.isFile()) {
+        const doc = await vscode.workspace.openTextDocument(
+          vscode.Uri.file(<string>fileOrFolder)
+        );
+        vscode.window.showTextDocument(doc);
+      } else if (stat.isDirectory()) {
+        util.getMyTerminal().sendText(`code ${fileOrFolder}`);
+      }
+    })
+  );
+}
+
+function registerCommandOpenMemoFile(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.my-openMemoFile", async () => {
+      const memoFilePath = "C:\\Users\\hw\\Desktop\\";
+
+      // 打开文件
+      let doc = await vscode.workspace.openTextDocument(
+        vscode.Uri.file(memoFilePath + "memo.txt")
+      );
+      vscode.window.showTextDocument(doc);
+
+      doc = await vscode.workspace.openTextDocument(
+        vscode.Uri.file(memoFilePath + "1.txt")
+      );
+      vscode.window.showTextDocument(doc);
+
+      doc = await vscode.workspace.openTextDocument(
+        vscode.Uri.file(memoFilePath + "2.txt")
+      );
+      vscode.window.showTextDocument(doc);
+    })
+  );
+}
+
+function registerCommandCreateAndOpenTempFile(
+  context: vscode.ExtensionContext
+) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("mytools.createTempFile", async () => {
+      // 临时文件名
+      const tempFileName =
+        (await vscode.window.showInputBox({ placeHolder: "Temp file name" })) ||
+        "";
+      if (isEmpty(tempFileName)) {
+        return;
+      }
+
+      const tempFilePath = path.normalize(path.join(os.tmpdir(), tempFileName));
+
+      // 若不存在就生成
+      if (!fs.existsSync(tempFileName)) {
+        fs.writeFileSync(tempFilePath, "");
+      }
+
+      // 打开文件
+      const doc = await vscode.workspace.openTextDocument(
+        vscode.Uri.file(tempFilePath)
+      );
+      vscode.window.showTextDocument(doc);
+    })
+  );
+}
+
+function registerCommandOpenFile(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("mytools.openfile", async () => {
+      const txt = await vscode.env.clipboard.readText();
+      const doc = await vscode.workspace.openTextDocument(vscode.Uri.file(txt));
+      vscode.window.showTextDocument(doc);
+    })
+  );
+}
+
+function registerCommandMarkdownToMediawiki(context: vscode.ExtensionContext) {
+  // 临时文件夹
+  const tempDir = os.tmpdir();
+  const tempFile = path.normalize(path.join(tempDir, "mytools-temp.txt"));
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.my-md2mediawiki", async () => {
+      // 系统剪贴板内容
+      const txt = await vscode.env.clipboard.readText();
+      if (isEmpty(txt)) {
+        return;
+      }
+
+      // 剪贴板内容到临时文件
+      fs.writeFileSync(tempFile, txt);
+
+      // pandoc转换
+      const targetExec = "pandoc --from gfm --to mediawiki " + tempFile;
+      const child = exec(
+        targetExec,
+        { cwd: tempDir },
+        function (error, stdout, stderr) {
+          if (error != null) {
+            vscode.window.showErrorMessage(error.message + "\n" + targetExec);
+          } else if (stderr != null && stderr != "") {
+            vscode.window.showErrorMessage(stderr + "\n" + targetExec);
+          } else {
+            vscode.env.clipboard.writeText(stdout);
+            vscode.window.showInformationMessage("Convert success");
+          }
         }
-    }));
+      );
+    })
+  );
 }
 
-function registerCommandOpenMemoFile(context: vscode.ExtensionContext)
-{
-    context.subscriptions.push(vscode.commands.registerCommand('extension.my-openMemoFile', async () => {
+function registerCommandMediawikiToMarkdown(context: vscode.ExtensionContext) {
+  // 临时文件夹
+  const tempDir = os.tmpdir();
+  const tempFile = path.normalize(path.join(tempDir, "mytools-temp.txt"));
 
-        const memoFilePath = "C:\\Users\\hw\\Desktop\\";
+  context.subscriptions.push(
+    vscode.commands.registerCommand("mytools.mediawiki2md", async () => {
+      // 系统剪贴板内容
+      let txt = await vscode.env.clipboard.readText();
+      if (isEmpty(txt)) {
+        return;
+      }
 
-        // 打开文件
-        var doc = await vscode.workspace.openTextDocument(vscode.Uri.file(memoFilePath + 'memo.txt'));
-        vscode.window.showTextDocument(doc);
+      txt = txt.replace(/</g, "ASDFGHJ");
+      txt = txt.replace(/>/g, "JHGFDSA");
 
-        doc = await vscode.workspace.openTextDocument(vscode.Uri.file(memoFilePath + '1.txt'));
-        vscode.window.showTextDocument(doc);
+      // 剪贴板内容到临时文件
+      fs.writeFileSync(tempFile, txt);
 
-        doc = await vscode.workspace.openTextDocument(vscode.Uri.file(memoFilePath + '2.txt'));
-        vscode.window.showTextDocument(doc);
-   }));
-}
+      // pandoc转换
+      const targetExec =
+        "pandoc --from mediawiki --to gfm --wrap=none " + tempFile;
+      const child = exec(
+        targetExec,
+        { cwd: tempDir },
+        function (error, stdout, stderr) {
+          if (error != null) {
+            vscode.window.showErrorMessage(error.message + "\n" + targetExec);
+          } else if (stderr != null && stderr != "") {
+            vscode.window.showErrorMessage(stderr + "\n" + targetExec);
+          } else {
+            //              ```
+            // **`Line1`**  Line1
+            // `Line2` to   Line2
+            // `Line3`      Line3
+            //              ```
+            let result = stdout.toString();
+            result = result.replace(/`[\s\r\n*]*`/g, "\n");
+            result = result.replace(/[*]*`/g, "\n```\n");
 
-function registerCommandCreateAndOpenTempFile(context: vscode.ExtensionContext)
-{
-    context.subscriptions.push(vscode.commands.registerCommand('extension.my-createTempFile', async () => {
+            result = result.replace(/ASDFGHJ/g, "<");
+            result = result.replace(/JHGFDSA/g, ">");
 
-        // 临时文件名
-        const tempFileName = await vscode.window.showInputBox({ placeHolder: "Temp file name" }) || "";
-        if (isEmpty(tempFileName)) {
-            return;
+            vscode.env.clipboard.writeText(result);
+            vscode.window.showInformationMessage("Convert success");
+          }
         }
-
-        var tempFilePath = path.normalize(path.join(os.tmpdir(), tempFileName));
-
-        // 若不存在就生成
-        if (!fs.existsSync(tempFileName)) {
-            fs.writeFileSync(tempFilePath, "");
-        }
-
-        // 打开文件
-        const doc = await vscode.workspace.openTextDocument(vscode.Uri.file(tempFilePath));
-        vscode.window.showTextDocument(doc);
-    }));
+      );
+    })
+  );
 }
-
-function registerCommandOpenFile(context: vscode.ExtensionContext)
-{
-    context.subscriptions.push(vscode.commands.registerCommand('extension.my-openfile', async () => {
-        const txt = await vscode.env.clipboard.readText();
-        const doc = await vscode.workspace.openTextDocument(vscode.Uri.file(txt));
-        vscode.window.showTextDocument(doc);
-    }));
-}
-
-function registerCommandMarkdownToMediawiki(context: vscode.ExtensionContext)
-{
-    // 临时文件夹
-    var tempDir = os.tmpdir();
-    var tempFile = path.normalize(path.join(tempDir, 'mytools-temp.txt'));
-
-    context.subscriptions.push(vscode.commands.registerCommand('extension.my-md2mediawiki', async () => {
-		// 系统剪贴板内容
-		const txt = await vscode.env.clipboard.readText();
-		if (isEmpty(txt)) {
-			return;
-		}
-
-        // 剪贴板内容到临时文件
-        fs.writeFileSync(tempFile, txt);
-
-        // pandoc转换
-        var targetExec = 'pandoc --from gfm --to mediawiki ' + tempFile;
-        var child = exec(targetExec, {cwd : tempDir}, function(error, stdout, stderr) {
-            if (error != null) {
-                vscode.window.showErrorMessage(error.message + '\n' + targetExec);
-            } else if (stderr != null && stderr != '') {
-                vscode.window.showErrorMessage(stderr + '\n' + targetExec);
-            } else {
-                vscode.env.clipboard.writeText(stdout);
-                vscode.window.showInformationMessage("Convert success");
-            }
-        });
-    }));
-}
-
-function registerCommandMediawikiToMarkdown(context: vscode.ExtensionContext)
-{
-    // 临时文件夹
-    var tempDir = os.tmpdir();
-    var tempFile = path.normalize(path.join(tempDir, 'mytools-temp.txt'));
-
-    context.subscriptions.push(vscode.commands.registerCommand('extension.my-mediawiki2md', async () => {
-
-		// 系统剪贴板内容
-		var txt = await vscode.env.clipboard.readText();
-		if (isEmpty(txt)) {
-			return;
-		}
-
-        txt = txt.replace(/</g, 'ASDFGHJ');
-        txt = txt.replace(/>/g, 'JHGFDSA');
-
-        // 剪贴板内容到临时文件
-        fs.writeFileSync(tempFile, txt);
-
-        // pandoc转换
-        var targetExec = 'pandoc --from mediawiki --to gfm --wrap=none ' + tempFile;
-        var child = exec(targetExec, {cwd : tempDir}, function(error, stdout, stderr) {
-            if (error != null) {
-                vscode.window.showErrorMessage(error.message + '\n' + targetExec);
-            } else if (stderr != null && stderr != '') {
-                vscode.window.showErrorMessage(stderr + '\n' + targetExec);
-            } else {
-
-                //              ```
-                // **`Line1`**  Line1
-                // `Line2` to   Line2
-                // `Line3`      Line3
-                //              ```
-                var result = stdout.toString();
-                result = result.replace(/`[\s\r\n*]*`/g, '\n');
-                result = result.replace(/[*]*`/g, '\n```\n');
-
-                result = result.replace(/ASDFGHJ/g, '<');
-                result = result.replace(/JHGFDSA/g, '>');
-        
-                vscode.env.clipboard.writeText(result);
-                vscode.window.showInformationMessage("Convert success");
-            }
-        });
-    }));
-}
-
- 
